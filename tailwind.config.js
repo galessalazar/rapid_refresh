36a9fdb80ad4df1b340014d6cd3370f1cb30ed49
Al Bundy <gale.s.salazar22@gmail.com>
2024-12-12 22:55:04 -0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  // ...
+}
