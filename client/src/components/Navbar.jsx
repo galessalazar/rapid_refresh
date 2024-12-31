b40680d073df7ad3323107ca7c80752dc29a4656
Al Bundy <gale.s.salazar22@gmail.com>
2024-12-30 18:19:09 -0600

    moved name to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,6 +37,9 @@ const Navbar = () => {
           <div className="hidden sm:ml-6 sm:block">
             <div className="flex space-x-4">
               {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+              {/* <Link to="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Rapid Refresh</Link> */}
+              <h1>Rapid Refresh!</h1>
+
               <Link to="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">About</Link>
               <Link to="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Services</Link>
               <Link to="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Pricing</Link>
