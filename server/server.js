2a3325b6db95e1849dba0e938a9d2802024869d1
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-07 21:13:12 -0600

    server running

--- server/server.js
+++ server/server.js
@@ -1,33 +1,27 @@
 const express = require("express");
 require("dotenv").config();
-const path = require('path');
-const db = require('./controllers/connection');
+const path = require("path");
+const db = require("./controllers/connection");
 const { Console } = require("console");
 
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-  res.send("");
-});
-
+  app.get("/", (req, res) => {
+    res.send("");
+  });
 }
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT} `)
-  })
+    console.log(`API server running on port ${PORT} `);
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
-});
 
