2195f077e2994025ec19bdba6a155369a8a5021a
Al Bundy <gale.s.salazar22@gmail.com>
2025-01-07 20:40:14 -0600

    added server.js

--- server/controllers/connection.js
+++ server/controllers/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+
+// double check the rapidrefresh part i dont remember where i get that from
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rapidrefresh');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 require("dotenv").config();
+const path = require('path');
+const db = require('./controllers/connection');
+const { Console } = require("console");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get("*", (req, res) => {
   res.send("");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+}
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT} `)
+  })
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
+});
+
