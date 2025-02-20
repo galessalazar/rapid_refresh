653c3364179fdae8ee4f78665ee609c858bbd489
galessalazar <gale.s.salazar22@gmail.com>
2025-02-19 18:24:46 -0600

    created service model

---
+++ server/models/Service.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../controllers/connection')
+
+class Service extends Model {}
+
+Service.init(
+    {
+        serviceName: {
+            type: DataTypes.STRING
+        },
+        serviceDescription: {
+            type: DataTypes.STRING
+        },
+        costOfService: {
+            type: DataTypes.INTEGER
+        },
+        estimatedTime: {
+            type: DataTypes.INTEGER
+        }
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'service'
+    }
+)
+
+module.exports = Service;
