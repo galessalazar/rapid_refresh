15498cf8ebe07b5f2e4ee349c2c69b6768e6b8a6
galessalazar <gale.s.salazar22@gmail.com>
2025-02-24 21:33:13 -0600

    separated dashboard and service form

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState} from "react";
 import axios from '../axiosConfig';
+import ServiceForm from "./ServiceForm";
 
 const Dashboard = () => {
     const [services, setServices] = useState([]);
-    setServices([...services, response.data]);
+    // setServices([...services, response.data]);
 
 
     useEffect(() => {
@@ -15,16 +16,22 @@ const Dashboard = () => {
         })
     }, []);
 
-   
+    const handleAddService = (newService) => {
+        setServices([...services, newService])
     }
-
-    const handleDelete = (id) => {
-        axios.delete(`/services/${id}`).then(() => {
-            setServices(services.filter(service => service.id !== id))
-        })
-
-        .catch(error => {
-            console.error('Error deleting service:', error);
-        }) 
-    }
+const handleDeleteService = (id) => {
+    setServices(services.filter(service => service.id !== id))
 }
+
+return (
+    <div>
+
+        <h2>Dashboard</h2>
+        <ServiceForm onAddService={handleAddService} />
+        <ServiceList services={services} onDeleteService={handleDeleteService} />
+    </div>
+)
+   
+}
+
+export default Dashboard;
