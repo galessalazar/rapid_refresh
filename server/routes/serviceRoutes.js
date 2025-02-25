4f2cffba3a350a062f4f51b348342b41126d477c
galessalazar <gale.s.salazar22@gmail.com>
2025-02-24 19:55:13 -0600

    post/put/delete routes created in serviceRoutes.js

--- server/routes/serviceRoutes.js
+++ server/routes/serviceRoutes.js
@@ -12,4 +12,62 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const { serviceName, serviceDescription, costOfService, estimatedTime } = req.body;
+
+        const newService = await Service.create({
+            serviceName,
+            serviceDescription,
+            costOfService,
+            estimatedTime
+        })
+        res.json(newService)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error')
+    }
+
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { serviceName, serviceDescription, costOfService, estimatedTime} = req.body;
+
+        const service = await Service.findByPk(id);
+        if (!service) {
+            return res.status(404).send('Service not found')
+        }
+
+        service.serviceName= serviceName || service.serviceName;
+        service.serviceDescription = serviceDescription || service.serviceDescription;
+        service.costOfService = costOfService || service.costOfService;
+        service.estimatedTime= estimatedTime || service.estimatedTime;
+
+        await service.save();
+        res.json(service);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error')
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const service = await Service.findByPk(id);
+
+        if (!Service) {
+            return res.status(404).send('Service not found');
+        }
+
+        await service.destroy();
+        res.json({ message: 'Service deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
