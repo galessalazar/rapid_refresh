b6fd816c3b1a45be685f436816e20c9049ad6a2b
galessalazar <gale.s.salazar22@gmail.com>
2025-02-25 19:32:20 -0600

    created proxy in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+
   server: {
     proxy: {
       "/api": {
@@ -13,5 +15,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
 });
