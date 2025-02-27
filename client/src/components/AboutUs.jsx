d7211e9da71a9eff8e241da5d326fbff7a62398e
galessalazar <gale.s.salazar22@gmail.com>
2025-02-27 17:22:13 -0600

    created about us page

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <h1>About Us</h1>
+      <p>Welcome to Rapid Refresh-Your Trusted Cleaning Partner!</p>
+      <p>
+        At Rapid Refresh, we understand that a clean space is a happy space.
+        That's why I am commited to providing top-notch cleaning services that
+        leave your home or business sparkling clean and stress-free. Whether
+        it's your home, office, or commercial space, I am here to handle the
+        dirty work so you can focus on what matters most.
+      </p>
+      <h2>Our Mission</h2>
+      <p>
+        At Rapid Refresh, we are driven by a simple mission:{" "}
+        <strong>
+          to provide thorough, high-quality cleaning services that make your
+          life easier.
+        </strong>{" "}
+        We believe in delivering honest, eco-friendlly cleaning solutions that
+        are safe for your family, pets, and the planet.
+      </p>
+      <h2>Why Choose Us?</h2>
+      <ul>
+        <li>
+          <strong>Experience Professionals:</strong>Our team is fully trained
+          and passionate about delivering exceptional cleaning results.
+        </li>
+        <li>
+          <strong>Eco-friendly:</strong>We use environmentally friendly cleaning
+          products that are safe for your family, pets, and the planet.
+        </li>
+        <li>
+          <strong>Customizable Services:</strong>From deep cleaning to regular
+          maintenance, we tailor our services to fit your needs and budget.
+        </li>
+        <li>
+          <strong>Satisfaction Guaranteed:</strong>We're not happy unless you're
+          happy. If you're not satisfied with our work, we'll make it right.
+        </li>
+      </ul>
+      <h2>Our Services</h2>
+      <ul>
+        <li>Residential Cleaning: Homes, apartments, and condos.</li>
+        <li>Commercial Cleaning: Offices, reatil spaces, and business locations.</li>
+        <li>Move-in/Move-out Cleaning: Make your transition easy with a thorough clean.</li>
+        <li>Deep Cleaning: A detailed, one-time service to get every nook and cranny.</li>
+        <li>Special Requests: Need something unique? We can customize our services to fit your </li>
+      </ul>
+      <h2>Get In Touch</h2>
+      <p>Have questions or want to schedule a cleaning? Feel free to <a href="/contact">Contact Us</a>! We'd love to help you create a cleaner, heathier space.</p>
+    </section>
+  );
+}
+
+export default AboutUs;
