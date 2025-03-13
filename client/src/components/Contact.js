2f1fb545713247320320b6c81566b92008e6cacb
galessalazar <gale.s.salazar22@gmail.com>
2025-03-13 16:10:39 -0500

    created contact component

---
+++ client/src/components/Contact.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const Contact = () => {
+    const [formData, setFormData] = useState ({
+        name: '',
+        email: '',
+        message: '',
+    })
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('Thank you for contacting us!');
+    };
+
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input
+                type="text"
+                id="name"
+                name='name'
+                value={formData.name}
+                onChange={handleChange}
+                required
+                />
+                <label htmlFor="message">Message:</label>
+                <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Contact;
