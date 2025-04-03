ed911ed7c1345dd8807665bba458bba3739b2f0d
galessalazar <gale.s.salazar22@gmail.com>
2025-04-03 12:23:49 -0500

    Added CORS for production

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
+const cors = require("cors");
 
 const sequelize = require("./controllers/connection");
 const serviceRoutes = require("./routes/serviceRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 // const { sequelize } = require('./models/index');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// restricts requests to only come from this frontend URL
+app.use(cors({
+  origin: 'https://rapid-refresh.vercel.app'
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,13 +24,14 @@ app.get("/", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  console.log(process.env.NODE_ENV);  // This will print 'production' or 'development'
+  console.log(process.env.NODE_ENV); // This will print 'production' or 'development'
 
   app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 // requests made to /services will go to serviceRoutes.js file, 1st parameter is building the url, 2nd parameter is the physical directory, this 1st parameter needs to match the axios post in serviceForm.jsx in the front
 app.use("/api/services", serviceRoutes);
-app.use('/api', contactRoutes);
+// still need to setup all configurations
+app.use("/api", contactRoutes);
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`API server running on port ${PORT} `));
