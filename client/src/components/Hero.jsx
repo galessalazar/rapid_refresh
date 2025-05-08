abab458ee942db9cca110662195854f991aca33a
galessalazar <gale.s.salazar22@gmail.com>
2025-05-08 09:27:47 -0500

    fixed the page layout with icons

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 // not sure i need to import the css
-import "../App.css"
+import "../App.css";
 const Hero = () => {
   return (
-    <section>
-      <div className=" w-full px-8 flex justify-evenly ">
-        <span className="material-symbols-outlined text-[200px]"> dry_cleaning</span>
-        <span className="material-symbols-outlined text-[200px]">local_laundry_service</span>
-        <span className="material-symbols-outlined text-[200px]"> cleaning </span>
-        <span className="material-symbols-outlined text-[200px]"> cleaning_services</span>
-
+    <section className="w-full px-8">
+      <div className="  flex justify-evenly ">
+        <span className="material-symbols-outlined text-[200px]">
+          {" "}
+          dry_cleaning
+        </span>
+        <span className="material-symbols-outlined text-[200px]">
+          local_laundry_service
+        </span>
+        <span className="material-symbols-outlined text-[200px]">
+          {" "}
+          cleaning{" "}
+        </span>
+        <span className="material-symbols-outlined text-[200px]">
+          {" "}
+          cleaning_services
+        </span>
       </div>
     </section>
   );
