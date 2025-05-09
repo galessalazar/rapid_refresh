db287a92e40d5784b23c36b990225863a89482c0
galessalazar <gale.s.salazar22@gmail.com>
2025-05-09 10:21:56 -0500

    link to services works

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import React from "react";
 // not sure i need to import the css
 import "../App.css";
@@ -5,12 +7,15 @@ const Hero = () => {
   return (
     <section className="w-full">
       <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
+        <Link to="/services">
         <div className="flex flex-col items-center gap-0.5">
           <p className="text-lg font-semibold">Laundry</p>
           <span className="material-symbols-outlined text-8xl sm:text-[200px]">
             dry_cleaning
           </span>
         </div>
+        </Link>
+        
         <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
           <div className="flex flex-col items-center gap-0.5">
             <p className="text-lg font-semibold">Dishes</p>
