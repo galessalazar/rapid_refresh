ed1a36b0e03a1965beb8729a487113925a277d77
galessalazar <gale.s.salazar22@gmail.com>
2025-05-09 10:26:43 -0500

    links all work

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,38 +8,43 @@ const Hero = () => {
     <section className="w-full">
       <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
         <Link to="/services">
-        <div className="flex flex-col items-center gap-0.5">
-          <p className="text-lg font-semibold">Laundry</p>
-          <span className="material-symbols-outlined text-8xl sm:text-[200px]">
-            dry_cleaning
-          </span>
-        </div>
+          <div className="flex flex-col items-center gap-0.5">
+            <p className="text-lg font-semibold">Laundry</p>
+            <span className="material-symbols-outlined text-8xl sm:text-[200px]">
+              dry_cleaning
+            </span>
+          </div>
         </Link>
-        
-        <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
-          <div className="flex flex-col items-center gap-0.5">
-            <p className="text-lg font-semibold">Dishes</p>
-            <span className="material-symbols-outlined text-8xl sm:text-[200px]">
-              local_laundry_service
-            </span>
+        <Link to="/services">
+          <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
+            <div className="flex flex-col items-center gap-0.5">
+              <p className="text-lg font-semibold">Dishes</p>
+              <span className="material-symbols-outlined text-8xl sm:text-[200px]">
+                local_laundry_service
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
-          <div className="flex flex-col items-center gap-0.5">
-            <p className="text-lg font-semibold">Sanitization</p>
-            <span className="material-symbols-outlined text-8xl sm:text-[200px]">
-              cleaning
-            </span>
+        </Link>
+        <Link to="/services">
+          <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
+            <div className="flex flex-col items-center gap-0.5">
+              <p className="text-lg font-semibold">Sanitization</p>
+              <span className="material-symbols-outlined text-8xl sm:text-[200px]">
+                cleaning
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
-          <div className="flex flex-col items-center gap-0.5">
-            <p className="text-lg font-semibold">Floor Cleaning</p>
-            <span className="material-symbols-outlined text-8xl sm:text-[200px]">
-              cleaning_services
-            </span>
+        </Link>
+        <Link to="/services">
+          <div className="flex flex-col items-center space-y-1 sm:flex-row justify-evenly">
+            <div className="flex flex-col items-center gap-0.5">
+              <p className="text-lg font-semibold">Floor Cleaning</p>
+              <span className="material-symbols-outlined text-8xl sm:text-[200px]">
+                cleaning_services
+              </span>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </section>
   );
