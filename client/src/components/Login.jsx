a199b36270c2429ea17d77d649b4821e0e6eaaf3
galessalazar <gale.s.salazar22@gmail.com>
2025-05-20 13:10:12 -0500

    login button created

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,98 +1,46 @@
-// import { useState } from "react";
-
-// const Login = () => {
-//   const [formState, setFormState] = useState({ email: "", password: "" });
-
-//   // update state based on form input changes
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-
-//     setFormState({
-//       ...formState,
-//       [name]: value,
-//     });
-//   };
-
-//   // submit form
-//   const handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     console.log("Form submitted:", formState);
-
-//     // clear form values
-//     setFormState({
-//       email: "",
-//       password: "",
-//     });
-//   };
-
-//   return (
-//     <main className="flex-row justify-center mb-4">
-//       <div className="col-12 col-lg-10">
-//         <div className="card">
-//           <h4 className="bruno-heading">Login</h4>
-//           <div className="card-body">
-//             <form onSubmit={handleFormSubmit}>
-//               <input
-//                 className="form-input"
-//                 placeholder="Your email"
-//                 name="email"
-//                 type="email"
-//                 value={formState.email}
-//                 onChange={handleChange}
-//               />
-
-//               <input
-//                 className="form-input"
-//                 placeholder="******"
-//                 name="password"
-//                 type="password"
-//                 value={formState.password}
-//                 onChange={handleChange}
-//               />
-//               <button
-//                 className="btn btn-block btn-info"
-//                 style={{ cursor: "pointer" }}
-//                 type="submit"
-//               >
-//                 Submit
-//               </button>
-//             </form>
-
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
-
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-
-      <input
-        type="email"
-        placeholder="Enter your email"
-        style={{
-          display: "block",
-          marginTop: "25px",
-          padding: "12px",
-          width: "100%",
-          maxWidth: "400px"
-        }}
-      ></input>
-      <input
-        type="password"
-        placeholder="Enter your password"
-        style={{
-          display: "block",
-          marginTop: "25px",
-          padding: "12px",
-          width: "100%",
-          maxWidth: "400px",
-        }}
-      ></input>
-    </div>
+    <form>
+      <div>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          style={{
+            display: "block",
+            marginTop: "25px",
+            padding: "12px",
+            width: "100%",
+            maxWidth: "400px",
+          }}
+        ></input>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          style={{
+            display: "block",
+            marginTop: "25px",
+            padding: "12px",
+            width: "100%",
+            maxWidth: "400px",
+          }}
+        ></input>
+        <button
+          type="submit"
+          style={{
+            marginTop: "25px",
+            padding: "12px",
+            width: "100%",
+            maxWidth: "400px",
+            backgroundColor: "#333",
+            color: "#fff",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          Login
+        </button>
+      </div>
+    </form>
   );
 };
 
