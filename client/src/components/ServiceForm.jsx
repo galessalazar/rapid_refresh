59e3b8cfb630ded25b535624bf0f50146c40e73c
galessalazar <gale.s.salazar22@gmail.com>
2025-07-20 08:53:42 -0500

    chore/addService button color update

--- client/src/components/ServiceForm.jsx
+++ client/src/components/ServiceForm.jsx
@@ -137,7 +137,7 @@ const ServiceForm = ({ onAddService }) => {
 
         <button
           type="submit"
-          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
           Add Service
         </button>
